b6734d29d1d6be6d7f541b56864171ab2560b4d4
jannes-m <malnamalja@gmx.de>
2018-04-20 16:18:05 +0200

    update freq_cits figure

--- code/01_cits_freq.R
+++ code/01_cits_freq.R
@@ -1,6 +1,7 @@
 # Filename: 01_cits_freq.R (2017-11-21)
 #
-# TO DO: Figure of publication frequency and times cited
+# TO DO: - Compare visually number of publications/citations between qualitative
+#          GIS and GIS research as a whole
 #
 # Author(s): Jannes Muenchow, Eric Krueger
 #
@@ -47,7 +48,7 @@ dim(pubs)  # 380
 pubs[duplicated(pubs$id_citavi), ]  # 0, perfect
 pubs[duplicated(pubs$WOS), ]  # 0, perfect
 
-# construct number of publication-per-year table
+# construct number of publication-per-year table (qualitative GIS)
 pubs_agg = group_by(pubs, year) %>%
   summarize(pubs = sum(length(year))) %>%
   mutate(pub_norm = pubs / sum(pubs)) %>%
@@ -69,6 +70,7 @@ pub_cits = melt(pub_cits, id.var = "year")
 names(pub_cits) = c("year", "class", "qual_gis")
 levels(pub_cits$class) = c("Publications", "Citations")
 
+# GIS research as a whole
 # compute portions
 gis_all = mutate(gis_all,
                  TC = TC / sum(TC),
@@ -76,35 +78,40 @@ gis_all = mutate(gis_all,
 gis_all = melt(gis_all, id.var = "PY")
 names(gis_all) = c("year", "class", "gis_all")
 levels(gis_all$class) = c("Citations", "Publications")
+# reverse level order
+gis_all$class = factor(gis_all$class, levels = c("Publications", "Citations"))
 
-
-# merge
+# merge qual_gis studies with all_gis studies
 d = full_join(gis_all, pub_cits)
 
-xyplot(gis_all + qual_gis ~ year | class, data = d, type = "b",
-       col = c("black", grey(0.5)), ylab = "Portions", xlab = c(),
-       aspect = "xy", layout = c(2, 1),
+p_1 = xyplot(gis_all + qual_gis ~ year | class, data = d, type = "b",
+       col = c("black", grey(0.5)),
+       ylab = list("Portions", cex = 0.8),
+       xlab = list("Year", cex = 0.8),
+       aspect = 0.75,
+       layout = c(2, 1),
        between = list(x = 0.5),
-       scales = list(# y = list(relation = "free"),
+       scales = list(
+         # y = list(relation = "free"),
+         # x = list(relation = "free"),
          tck = c(1, 0),
          alternating = c(1, 1)),
-       strip = strip.custom(bg = 'white'),
+       strip = strip.custom(bg = "white"),
+       par.strip.text = list(cex = 0.8),
        #Create your own legend
-       key = list(space = "right", text = list(c("all", "qual"), cex = 0.8),
+       key = list(space = "right", columns = 1,
+                  text = list(c("GIS", "qualitative\nGIS"), cex = 0.6),
                   lines = list(col = c("black", grey(0.5)), lwd = 2,
                                cex = 0.6))
 )
 
-
-png(filename = file.path(dir_fig, "freq_cits.png"), width = 14,
+# save figure
+png(filename = "figures/freq_cits.png", width = 18,
     height = 8, units = "cm", res = 300)
-p_1
+print(p_1)
+grid.text("a)", x = unit(0.12, "npc"), y = unit(0.825, "npc"),
+          gp = gpar(font = "bold", fontsize = 10))
+grid.text("b)", x = unit(0.46, "npc"), y = unit(0.825, "npc"),
+          gp = gpar(font = "bold", fontsize = 10))
 dev.off()
 
-# grid.text("a)", x = unit(0.125, "npc"), y = unit(0.765, "npc"),
-#           gp = gpar(font = "bold", fontsize = 10))
-# grid.text("b)", x = unit(0.49, "npc"), y = unit(0.765, "npc"),
-#           gp = gpar(font = "bold", fontsize = 10))
-
-
-# cols: year, all_gis, qual_gis, class (2 levels: Publications, Citations)
