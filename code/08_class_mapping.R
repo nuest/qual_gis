fda4f39e6e8c2dac2bbd78c88148f50a485c8137
jannes-m <malnamalja@gmx.de>
2018-04-27 12:45:36 +0200

    update class mapping

--- code/08_class_mapping.R
+++ code/08_class_mapping.R
@@ -1,4 +1,4 @@
-# Filename: 02-class_mapping.R (2017-09-14)
+# Filename: 08_class_mapping.R (2018-04-25)
 #
 # TO DO: map kmeans classes
 #
@@ -9,45 +9,44 @@
 #**********************************************************
 #
 # 1. ATTACH PACKAGES AND DATA
-# 2. 
+# 2. MAPPING CLUSTERS
 #
 #**********************************************************
 # 1 ATTACH PACKAGES AND DATA-------------------------------
 #**********************************************************
 
 # attach packages
+library("dplyr")
 library("sp")
 library("spData")
 library("sf")
 library("lattice")
 library("latticeExtra")
 
-# define directories
-dir_main = "."
-dir_data = file.path(dir_main, "data")
-dir_ima = file.path(dir_main, "images")
-dir_figs = file.path(dir_main, "figures")
-
 # attach data
-load(file.path(dir_ima, "01_input.Rdata"))
-load(file.path(dir_ima, "07_mat.Rdata"))
-load(file.path(dir_ima, "07_classes.Rdata"))
+qual = readRDS("images/00_qual.rds")
+wos = readRDS("images/00_wos.rds")
+ind = readRDS("images/07_ind.rds")
+ord = readRDS("images/07_ord.rds")
+classes = readRDS("images/07_classes.rds")
+# does not work without the data
+p_1 = readRDS("images/07_p_1.rds")
 
 #**********************************************************
 # 4 MAPPING CLUSTERS---------------------------------------
 #**********************************************************
 
-# rownames correspond to idCitavi
+# rownames correspond to id_citavi
 clus = data.frame(cluster = classes$cluster)
-clus$idCitavi = as.numeric(rownames(clus))
+clus$id_citavi = as.numeric(rownames(clus))
 # sum(rownames(clus) == rownames(mat)) == nrow(mat)  # TRUE, perfect
-setdiff(clus$idCitavi, qual$fidCitavi)
+setdiff(clus$id_citavi, qual$fid_citavi)
 # 13 manuscripts without an abstract
-setdiff(qual$fidCitavi, clus$idCitavi)
-clus = dplyr::inner_join(clus, 
-                  dplyr::select(qual, fidCitavi, lat = Latitude,
+setdiff(qual$fid_citavi, clus$id_citavi)
+clus = inner_join(clus,
+                  dplyr::select(qual, fid_citavi, lat = Latitude,
                                 lon = Longtitude),
-                  by = c("idCitavi" = "fidCitavi"))
+                  by = c("id_citavi" = "fid_citavi"))
 head(clus)
 clus[clus$lon > 180 | clus$lon < -180, ]
 clus[clus$lon > 180 | clus$lon < -180, "lon"] = -12.31
@@ -57,19 +56,23 @@ dim(clus[clus$lon == 0 & clus$lat == 0, ])  # 100 studies without coordinates
 # remove them
 clus = clus[!(clus$lon == 0 & clus$lat == 0), ]
 
-plot(st_geometry(world), ylim = c(-30, 30), xlim = c(-140, 140))
-points(clus$lon, clus$lat, pch = 16, col = clus$cluster)
+plot(st_geometry(world), ylim = c(-30, 30), xlim = c(-140, 170))
+clus$col = as.factor(clus$cluster)
+# use the exact same palette as in 07_word_ordi_cluster.R
+pal = RColorBrewer::brewer.pal("Set3", n =  6)[3:6]
+levels(clus$col) = pal
+points(clus$lon, clus$lat, pch = 16, col = as.character(clus$col))
+legend(0, -45, legend = paste("cluster", 1:4), fill = pal, ncol = 2)
 # check if there were really studies in Hawaii and Tahiti
-
-ind = filter(clus, lon < -129 & lon > -160) %>% dplyr::pull(idCitavi)
-filter(qual, fidCitavi %in% ind)
-filter(wos, idCitavi %in% c(109, 448))
-# ok, first article is located in Hawaii, second in Polyesia, perfect
+ind = filter(clus, lon < -129 & lon > -160) %>% dplyr::pull(id_citavi)
+filter(qual, fid_citavi %in% ind)
+filter(wos, id_citavi %in% c(109, 448))
+# ok, first article is located in Hawaii, second in Polynesia, perfect
 
 
 x = clus[, c("lon", "lat", "cluster")]
 coordinates(clus) =~ lon + lat
-pal = RColorBrewer::brewer.pal("Set3", n =  6)[3:6]
+
 world = spTransform(as(world, "Spatial"), "+proj=wintri")
 proj4string(clus) = "+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0"
 clus = spTransform(clus, "+proj=wintri")
@@ -101,18 +104,18 @@ spplot(clus, "cluster", col.regions = pal, border = "black", cex = 0.75,
 # loadMeuse()
 # levelplot(zinc~x+y, as.data.frame(meuse), panel=panel.levelplot.points,
 #           aspect = "iso", scales = list(draw=FALSE), xlab=NULL, ylab = NULL,
-#           colorkey = list(space = "bottom", width = 0.2, height = 2)) 
+#           colorkey = list(space = "bottom", width = 0.2, height = 2))
 # levelplot(cluster~lat+lon, as.data.frame(clus), panel = panel.levelplot.points,
 #           apsect = "iso", scales = list(draw=FALSE), xlab=NULL, ylab = NULL,
 #           colorkey = list(space = "bottom", width = 2, height = 0.2))
 
 
 # in fact, spplot.points uses xyplot (see github sp)
-xyplot(coordinates(clus)[, 2] ~ coordinates(clus)[, 1], asp = "iso", col = pal, 
-       pch = 20, 
+xyplot(coordinates(clus)[, 2] ~ coordinates(clus)[, 1], asp = "iso", col = pal,
+       pch = 20,
        key = list(points = list(fill = pal, pch = 21,
                                 border = "black", cex = 2),
-                  space = "right", 
+                  space = "right",
                   text = list(paste("cluster", 1:4))))
 
 map = spplot(clus, "cluster", col.regions = pal, cex = 1.2,
@@ -120,7 +123,7 @@ map = spplot(clus, "cluster", col.regions = pal, cex = 1.2,
              key = list(
                points = list(fill = pal, pch = 21, border = "black",
                              cex = 1.5),
-               text = list(paste("cluster", 1:4), cex = 0.8)
+               text = list(paste(c("EL", "MT", "PC", "UI"), "cluster"), cex = 0.8)
              ),
              sp.layout = list(
                # list("sp.points", clus, col = "black", pch = 21, cex = 1.2),
@@ -128,7 +131,9 @@ map = spplot(clus, "cluster", col.regions = pal, cex = 1.2,
                     first = TRUE)
              )
 )
-png(filename = file.path(dir_figs, "map_cluster.png"), res = 300,
+
+# save your result
+png(filename = "figures/map_cluster.png", res = 300,
     width = 17, height = 8, units = "cm")
 print(map)
 dev.off()
